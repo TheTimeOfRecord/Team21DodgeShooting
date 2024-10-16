5e267b08ee8f9f9162bebc7df683983231fdc91b
Zzondy-Unity <abdos230@naver.com>
2024-10-16 21:02:33 +0900

    Reapply "Merge branch 'Dev' into Feature_LYG"
    This reverts commit e0701f886cd2d82f07ccc452b361cce036f9e508.

---
+++ Assets/Scripts/Enemy/BlinkingEnemyController.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class BlinkingEnemyController : EnemyController
+{
+    private bool canBlink;
+
+    protected override void Start()
+    {
+        base.Start();
+        canBlink = true;
+    }
+
+    protected override void FixedUpdate()
+    {
+        Vector2 direction = DirectionToTarget();
+        RotateToTarget(direction);
+
+        if (canBlink)
+        {
+            StartCoroutine(BlinkCoroutine());
+        }
+    }
+
+    IEnumerator BlinkCoroutine()
+    {
+        Blink();
+        canBlink = false;
+        yield return new WaitForSeconds(3f);
+        canBlink = true;
+    }
+
+    private void Blink()
+    {
+        float x;
+        float y;
+        while (true)
+        {
+            x = Random.Range(-5f, 5f);
+            y = Random.Range(-3f, 7.5f);
+
+            if ((x > -1 && x < 1) && (y > -1 && y < 1))
+            {
+                continue;
+            }
+            else
+            {
+                break;
+            }
+        }
+        transform.position = PlayerTransform.position + new Vector3(x, y, 0f);
+    }
+}
---
+++ Assets/Scripts/Enemy/HoveringEnemyController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using UnityEngine;
+
+public class HoveringEnemyController : EnemyController
+{
+    [SerializeField, Range(1f, 5f)] private float Range;
+    private float rad;
+    private bool isRanged = false;
+    private Vector2 initDirectionInRange;
+
+    protected override void Start()
+    {
+        base.Start();
+    }
+
+    protected override void FixedUpdate()
+    {
+        base.FixedUpdate();
+        Vector2 direction = DirectionToTarget();
+
+        RotateToTarget(direction);
+
+        if (!isRanged)
+        {
+            float distance = DistanceToTarget();
+            CallMoveEvent(direction);
+
+            if (distance <= Range)
+            {
+                isRanged = true;
+                initDirectionInRange = direction * -1;
+            }
+        }
+        else
+        {
+            direction = direction * -1;
+            float rotZ = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+            transform.localRotation = Quaternion.Euler(0, 0, rotZ);
+            MoveCircle();
+        }
+    }
+
+    private void MoveCircle()
+    {
+        rad += Time.deltaTime * (speed/Range);
+        float initialRad = Mathf.Atan2(initDirectionInRange.y, initDirectionInRange.x);
+        float x = Range * Mathf.Cos(rad + initialRad);
+        float y = Range * Mathf.Sin(rad + initialRad);
+        transform.position = PlayerTransform.position + new Vector3(x, y);
+        if (rad >= Mathf.PI * 2) rad = 0;
+    }
+}
