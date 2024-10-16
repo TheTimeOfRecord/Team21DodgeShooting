47c09ade8edf76358c020f3deda1475606716f90
Zzondy-Unity <abdos230@naver.com>
2024-10-16 21:02:39 +0900

    Reapply "Merge branch 'Dev' into Feature_LYG"
    This reverts commit 930ec2c81ae9ef50cdc5ea4b4ed59e866c131011.

---
+++ Assets/Scripts/Enemy/TracingEnemyController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using UnityEngine;
+
+public class TracingEnemyController : EnemyController
+{
+    private bool isCollidingWithTarget;
+
+    protected override void Start()
+    {
+        base.Start();
+    }
+
+    protected override void FixedUpdate()
+    {
+        base.FixedUpdate();
+
+        Vector2 direction = DirectionToTarget();
+        CallMoveEvent(direction);
+        RotateToTarget(direction);
+    }
+}
