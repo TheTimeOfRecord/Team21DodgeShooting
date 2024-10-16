e34aad8e36d3ceb9ff6d6a006dde0ae5a579bbff
hoshi <chlehdrb4967@gmail.com>
2024-10-16 09:05:21 +0900

    Update EnemyBasicProjectile.cs

--- Assets/Scripts/EnemyProjectile/EnemyBasicProjectile.cs
+++ Assets/Scripts/EnemyProjectile/EnemyBasicProjectile.cs
@@ -5,17 +5,19 @@ using UnityEngine;
 public class EnemyBasicProjectile : MonoBehaviour
 {
     public float speed = 5f;
-    
+
     public Vector2 direction = Vector2.down;
+
+    private Rigidbody2D rb;
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
-    
+
     void Update()
     {
-        transform.Translate(direction * speed * Time.deltaTime);
+        rb.velocity = direction * speed;
     }
     void OnBecameInvisible()
     {
