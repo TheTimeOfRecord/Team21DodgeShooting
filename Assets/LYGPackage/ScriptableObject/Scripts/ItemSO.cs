faa914e32cdd597763e9ffd2b9ec207a2c58e107
TheTimeOfRecord <131391796+TheTimeOfRecord@users.noreply.github.com>
2024-10-17 11:15:52 +0900

    Asset LYGPackage
    이전 작업물들을 LYGPackage 로 가져옴

---
+++ Assets/LYGPackage/ScriptableObject/Scripts/ItemSO.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public enum ItemType 
+{ 
+    //Usable Item
+    RecoveryPotion = 0,
+    Bomb,
+
+    //StatModifier
+    IncreaseBulletNum = 100,
+    IncreaseBulletDamage,
+    IncreaseSpeed,
+    IncreaseHealth
+}
+
+[CreateAssetMenu(fileName ="ItemSO", menuName = "DodgeController/Items/ItemSO")]
+public class ItemSO : ScriptableObject
+{
+    [Header("Base Info")]
+    public Image itemImage;
+    public ItemType itemType;
+    public Color itemBackColor;
+    public Color fontColor;
+    public string itemInfo;
+    public float stat;
+    
+}
