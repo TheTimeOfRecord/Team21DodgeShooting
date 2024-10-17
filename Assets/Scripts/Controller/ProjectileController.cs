29cd739472975d73ee6f4cb404801268172933e2
Zzondy-Unity <abdos230@naver.com>
2024-10-17 20:15:38 +0900

    Update Projectiles
    Standard, Homing, Pierce, Spread Bullets Scripts and Prefabs

--- Assets/Scripts/Controller/ProjectileController.cs
+++ Assets/Scripts/Controller/ProjectileController.cs
@@ -5,13 +5,47 @@ using UnityEngine;
 
 public class ProjectileController : MonoBehaviour
 {
-    //LayerMask를 리턴?, GameObject를 리턴?
-    [SerializeField] private LayerMask layerMask;
+    [SerializeField] private LayerMask bulletLayer;
+    [SerializeField] private LayerMask EnemyLayer;
+    [SerializeField] private LayerMask PlayerLayer;
+
+    private Collider2D thisCollider;
+
+    private void Awake()
+    {
+        thisCollider = GetComponent<Collider2D>();
+    }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        
+        Bullet bullet = GetComponent<Bullet>();
+
+        //총알을 쏜 주체가 맞게되는 경우
+        if (collision.gameObject == bullet.shooter)
+        {
+            Physics2D.IgnoreCollision(collision, thisCollider);
+            return;
+        }
+
+        //상대방 혹은 플레이어가 맞을경우
+        if (IsLayerMatched(EnemyLayer.value, collision.gameObject.layer))
+        {
+            bullet.OnImpact(collision);
+        }
+        else if(IsLayerMatched(PlayerLayer.value, collision.gameObject.layer))
+        {
+            bullet.OnImpact(collision);
+        }
+
+        //총알끼리 부딪힌 경우
+        else if (IsLayerMatched(bulletLayer.value, collision.gameObject.layer))
+        {
+            bullet.DestroyProjectile();
+        }
     }
 
-
+    private bool IsLayerMatched(int layerMask, int layer)
+    {
+        return layerMask == (layerMask | 1 << layer);
+    }
 }
--- Assets/Scripts/Projectile/Bullet.cs
+++ Assets/Scripts/Projectile/Bullet.cs
@@ -2,15 +2,40 @@ using UnityEngine;
 
 public abstract class Bullet : MonoBehaviour
 {
-    public abstract void Move(float speed, float damage);
-    public abstract void OnImpact(Collider2D collision);
+    public GameObject shooter { get; private set; }
+
+    //데미지처리, 움직임, 사이크조절 다 따로하는게 좋아보인다.
+    public abstract void Move(float speed, Vector2 target);
+    public virtual void OnImpact(Collider2D collision)
+    {
+        //해당 기체에 대미지를 준다.
+        HealthSystem healthSystem = collision.gameObject.GetComponent<HealthSystem>();
+        StatHandler statHandler = GetComponent<StatHandler>();
+        healthSystem.ChangeHealth(statHandler.CurrentStat.ATK);
+        DestroyProjectile();
+    }
+
+    public void SetShooter(GameObject _shooter)
+    {
+        shooter = _shooter;
+    }
+
+    public void SetBulletSize(float size)
+    {
+        transform.localScale = Vector3.one * size;
+    }
+
+    public void DestroyProjectile()
+    {
+        gameObject.SetActive(false);
+    }
 }
 
 public enum BulletType
 {
     StandardBullet,
     PierceBullet,
-    HomingBullet,
+    HomingBullet,   //속도의 0.1배
     CircleBullet
 }
 
