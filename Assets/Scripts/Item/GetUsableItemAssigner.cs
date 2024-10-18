a53830b91e82caead29a026ef22c1d2b749bc9dd
TheTimeOfRecord <131391796+TheTimeOfRecord@users.noreply.github.com>
2024-10-18 20:14:05 +0900

    Feature GetUsableItemAssigner
    GetUsableItemAssigner 를 추가하여 ItemSO 타입이 UsableItem인 아이템을 획득했을 때의 로직을 작성하였다.
    ItemToStatModifier와 동일하게 Dictionary로 관리된다.

---
+++ Assets/Scripts/Item/GetUsableItemAssigner.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GetUsableItemAssigner : MonoBehaviour
+{
+    [SerializeField] private HealthSystem playerHealthSystem;
+    private Dictionary<string, Action<float, HealthStatSO>> getUsableItemActions;
+    private void Awake()
+    {
+        getUsableItemActions = new Dictionary<string, Action<float, HealthStatSO>>()
+        {
+            {"RecoveryPotion", GetRecoveryPotion },
+            {"Bomb", GetBomb}
+        };
+    }
+
+    public void GetRecoveryPotion(float itemStat, HealthStatSO currentStat)
+    {
+        playerHealthSystem.ChangeHealth(itemStat);
+        Debug.Log("Use RecoveryPotion");
+    }
+    public void GetBomb(float itemStat, HealthStatSO currentStat)
+    {
+        // TODO : itemStat으로 폭탄의 데미지를 조정한다.
+        currentStat.bombNum += 1;
+        Debug.Log("Use Bomb");
+    }
+
+}
