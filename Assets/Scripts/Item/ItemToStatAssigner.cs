2a4f08e357fd9316f7793ceb52fc8ff2aab7b216
TheTimeOfRecord <131391796+TheTimeOfRecord@users.noreply.github.com>
2024-10-18 20:12:07 +0900

    Feature ItemToStatAssigner
    ItemToStatAssigner를 추가하여 StatModifier 타입의 ItemSO가 들어왔을 때, 각 ItemSO의 이름에 맞게 currentStat을 변경해주는 로직을 추가하였다.
    해당 로직은 Dictionary로 관리된다.

---
+++ Assets/Scripts/Item/ItemToStatAssigner.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemToStatAssigner : MonoBehaviour
+{
+    private Dictionary<string, Action<HealthStatSO, float>> statModifiers;
+
+    private void Awake()
+    {
+        statModifiers = new Dictionary<string, Action<HealthStatSO, float>>
+        {
+            {"IncreaseBulletNum", (stat, value) => stat.bulletNum += (int)value },
+            {"IncreaseBulletDamage", (stat, value) => stat.ATK += value },
+            {"IncreaseSpeed", (stat, value) => stat.speed += value },
+            {"IncreaseHealth", (stat, value) => stat.maxHP += value }
+            //StatModifier추가시 여기에 기능 추가
+        };
+    }
+
+    public void ModifyStatBasedOnItem(ItemSO itemSO, HealthStatSO currentStat)
+    {
+        if (itemSO.itemType == ItemType.StatModifier)
+        {
+            if (statModifiers.TryGetValue(itemSO.itemName, out Action<HealthStatSO, float> modifyAction))
+            {
+                modifyAction(currentStat, itemSO.stat);
+                Debug.Log("스탯변경 완료.");
+            }
+            else
+            {
+                Debug.Log($"{itemSO.itemName}에 대한 스탯 변경 규칙이 정의되지 않았습니다.");
+            }
+        }
+    }
+}
