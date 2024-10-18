e15bafcd4687c91617faed7d93dafa0432227929
TheTimeOfRecord <131391796+TheTimeOfRecord@users.noreply.github.com>
2024-10-18 12:03:18 +0900

    Refactor ItemSO
    ItemSO의 enum ItemType을 통해 아이템 종류를 관리를 하였는데, ItemSO의 이름을 가져와서 itemType으로 사용하도록 변경하였다.
    따라서, ItemSO만 추가하면 자동으로 아이템 타입이 정해진다.

--- Assets/ScriptableObject/Items/Scripts/ItemSO.cs
+++ Assets/ScriptableObject/Items/Scripts/ItemSO.cs
@@ -2,28 +2,20 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public enum ItemType 
-{ 
-    //Usable Item
-    RecoveryPotion = 0,
-    Bomb,
-
-    //StatModifier
-    IncreaseBulletNum = 100,
-    IncreaseBulletDamage,
-    IncreaseSpeed,
-    IncreaseHealth
-}
-
 [CreateAssetMenu(fileName ="ItemSO", menuName = "DodgeController/Items/ItemSO")]
 public class ItemSO : ScriptableObject
 {
     [Header("Base Info")]
     public Sprite itemSprite;
-    public ItemType itemType;
+    public string itemType;
     public Color itemBackColor;
     public Color fontColor;
     public string itemInfoText;
     public string statInfoText;
     public float stat;
+
+    private void OnValidate()
+    {
+        itemType = this.name;
+    }
 }
